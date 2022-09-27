dd22deaa114f09d729570c86e9a105fa28487be2
Jomkie <937574740@qq2.com>
2022-09-27 13:44:48 +0800

    整理

--- src/main/java/com/jomkie/sm/JomkieSmApplication.java
+++ src/main/java/com/jomkie/sm/JomkieSmApplication.java
@@ -1,6 +1,5 @@
 package com.jomkie.sm;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.jomkie.sm.entity.User;
 import com.jomkie.sm.mapper.UserMapper;
 import org.apache.ibatis.session.SqlSession;
@@ -8,16 +7,16 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Properties;
 
 public class JomkieSmApplication {
 
     public static void main(String[] args) throws IOException {
-        InputStream inputStream = new FileInputStream(JomkieSmApplication.class.getResource("/").getPath() + "/mybatis-config.xml");
+        String rootPath = JomkieSmApplication.class.getResource("/").getPath();
+        System.out.println(rootPath);
+        InputStream inputStream = new FileInputStream(rootPath + "/mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
