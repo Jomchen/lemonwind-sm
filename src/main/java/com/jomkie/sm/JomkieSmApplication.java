1b69d091630c38aeb94632bee7d03968eca10e10
Jomkie <937574740@qq2.com>
2022-09-28 09:46:09 +0800

    增加

--- src/main/java/com/jomkie/sm/JomkieSmApplication.java
+++ src/main/java/com/jomkie/sm/JomkieSmApplication.java
@@ -7,30 +7,33 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class JomkieSmApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchMethodException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        List<User> userList = userMapper.findAll(10, 20, 1);
-        for (User user : userList) {
-            System.out.println(user.toString());
-        }
+
+        List<User> userList;
 
         System.out.println("-------------------------------------------------------------");
         userList = userMapper.findCondition(6, 15, 0);
-        for (User user : userList) {
-            System.out.println(user.toString());
+        printAll(userList);
+
+    }
+
+    public static void printAll(List<User> list) {
+        if (null == list || list.isEmpty()) {
+            return;
         }
 
+        list.stream().forEach(System.out::println);
     }
 
 }
